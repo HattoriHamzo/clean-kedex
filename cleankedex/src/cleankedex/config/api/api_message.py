36a5746571a4b4240dae50eea7349977f2f7e956
HattoriHamzo <77831591+HattoriHamzo@users.noreply.github.com>
2023-12-24 16:24:41 +0100

    Delete cleankedex directory

--- cleankedex/src/cleankedex/config/api/api_message.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ApiMessage(BaseModel):
-    """Model representing a generic API message with a message and error code."""
-
-    message: str
-    error_code: int
