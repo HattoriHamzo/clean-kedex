8b42e557d6ec89071c90d166a407e4749524d85f
HattoriHamzo <pablo@Pablos-MBP.home>
2023-12-27 13:18:37 +0100

    cleaned tests

--- cleankedex/tests/api/v1/pokemon/test_pokemon_api.py
+++ cleankedex/tests/api/v1/pokemon/test_pokemon_api.py
@@ -12,13 +12,25 @@ from cleankedex.main import app
 # Config
 client: TestClient = TestClient(app)
 add_pagination(app)
+
+# Urls
+base_url_server: str = "http://testserver"
 endpoint_get_pokemons: str = "/api/v1/pokemons"
 endpoint_get_pokemons_by_id: str = "/api/v1/pokemons/1"
+
+# patch urls
+pokemon_read_service_mock: str = (
+    "cleankedex.api.pokemon.v1.pokemon_api.PokemonReadService"
+)
+
+# Exception messages
+generic_not_controlled_exception: str = "Not controlled exception"
+generic_not_found_exception: str = "Not found exception"
 
 
 @pytest.mark.asyncio
 async def test_get_pokemons() -> None:
-    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+    async with httpx.AsyncClient(app=app, base_url=base_url_server) as client:
         response = await client.get(endpoint_get_pokemons)
 
     assert response.status_code == 200
@@ -26,12 +38,10 @@ async def test_get_pokemons() -> None:
 
 @pytest.mark.asyncio
 async def test_get_pokemons_not_controlled_exception():
-    with patch(
-        "cleankedex.api.pokemon.v1.pokemon_api.PokemonReadService"
-    ) as mock_get_pokemons:
-        mock_get_pokemons.side_effect = Exception("Not controlled exception")
+    with patch(pokemon_read_service_mock) as mock_get_pokemons:
+        mock_get_pokemons.side_effect = Exception(generic_not_controlled_exception)
 
-        async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        async with httpx.AsyncClient(app=app, base_url=base_url_server) as client:
             response = await client.get(endpoint_get_pokemons)
 
     assert response.status_code == 500
@@ -39,7 +49,7 @@ async def test_get_pokemons_not_controlled_exception():
 
 @pytest.mark.asyncio
 async def test_get_pokemon_by_id() -> None:
-    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+    async with httpx.AsyncClient(app=app, base_url=base_url_server) as client:
         response = await client.get(endpoint_get_pokemons_by_id)
 
     assert response.status_code == 200
@@ -47,14 +57,12 @@ async def test_get_pokemon_by_id() -> None:
 
 @pytest.mark.asyncio
 async def test_get_pokemon_by_id_pokemon_not_found_exception():
-    with patch(
-        "cleankedex.api.pokemon.v1.pokemon_api.PokemonReadService"
-    ) as mock_get_pokemons:
+    with patch(pokemon_read_service_mock) as mock_get_pokemons:
         mock_get_pokemons.side_effect = PokemonNotFoundException(
-            "Not controlled exception"
+            generic_not_found_exception
         )
 
-        async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        async with httpx.AsyncClient(app=app, base_url=base_url_server) as client:
             response = await client.get(endpoint_get_pokemons_by_id)
 
     assert response.status_code == 404
@@ -62,12 +70,10 @@ async def test_get_pokemon_by_id_pokemon_not_found_exception():
 
 @pytest.mark.asyncio
 async def test_get_pokemon_by_id_not_controlled_exception():
-    with patch(
-        "cleankedex.api.pokemon.v1.pokemon_api.PokemonReadService"
-    ) as mock_get_pokemons:
-        mock_get_pokemons.side_effect = Exception("Not controlled exception")
+    with patch(pokemon_read_service_mock) as mock_get_pokemons:
+        mock_get_pokemons.side_effect = Exception(generic_not_controlled_exception)
 
-        async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        async with httpx.AsyncClient(app=app, base_url=base_url_server) as client:
             response = await client.get(endpoint_get_pokemons_by_id)
 
     assert response.status_code == 500
